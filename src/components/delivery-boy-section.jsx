594d128c37e992b392936f05659a3810f34561f3
Neo.Eskini <mreskini30@gmail.com>
2021-08-22 22:35:39 +0430

    blueprint of the delivery-boy-section is now ready

--- src/components/delivery-boy-section.jsx
+++ src/components/delivery-boy-section.jsx
@@ -3,7 +3,7 @@ import deliveryBoyImage from "../assets/img/delivery-boy.png"
 const DeliveryBoy = () => {
     return (
         <div className="py-5">
-            <div className="theme-orange-color display-2 font-weight-bold">
+            <div className="theme-orange-color display-3 font-weight-bold">
                 Customers to get delivered their ordered foods
             </div>
             <div className="row py-5">
@@ -11,19 +11,19 @@ const DeliveryBoy = () => {
                     <img src={deliveryBoyImage} className="img-fluid pr-3" alt="Delivery Boy Image" />
                 </div>
                 <div className="col-lg-6 my-auto">
-                    <h4 className="theme-title-color font-weight-600">
+                    <h4 className="theme-title-color font-weight-600 my-3">
                         Menu Management
                     </h4>
                     <p className="theme-content-color font-weight-400 line-height-30">
                         Effort into each unique project knowing that the more we put into it, the more we get out of it.
                     </p>
-                    <h4 className="theme-title-color font-weight-600">
+                    <h4 className="theme-title-color font-weight-600 my-3">
                         Food Management
                     </h4>
                     <p className="theme-content-color font-weight-400 line-height-30">
                         Effort into each unique project knowing that the more we put into it, the more we get out of it.
                     </p>
-                    <h4 className="theme-title-color font-weight-600">
+                    <h4 className="theme-title-color font-weight-600 my-3">
                         Delivery Management
                     </h4>
                     <p className="theme-content-color font-weight-400 line-height-30">
