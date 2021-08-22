0c58e84b5e6c504917294eae22732971bee1d0b2
Neo.Eskini <mreskini30@gmail.com>
2021-08-22 19:19:23 +0430

    added the react-bootstrap dependency thing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from "./app"
 
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min'
 
 ReactDOM.render(
   <React.StrictMode>
