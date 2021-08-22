053ae1e05d2faeb15d9b1b57dd7a146cd457da72
Neo.Eskini <mreskini30@gmail.com>
2021-08-22 19:09:40 +0430

    added the bootstrap css and js bundle file things tot he index.js file thing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from "./app"
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
