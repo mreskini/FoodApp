c18fecae5fe6cfdc0ff7656a6751036f74eb26b8
Neo.Eskini <mreskini30@gmail.com>
2021-08-22 19:07:03 +0430

    added the bootstrap dependency thing

--- src/components/delivery-boy-section.jsx
+++ src/components/delivery-boy-section.jsx
@@ -2,8 +2,8 @@ import ComponentWrapper from "./component-wrapper"
 
 const DeliveryBoy = () => {
     return (
-        <div>
-            Mohammad
+        <div className="theme-orange-color display-1">
+            Customers to get delivered their ordered foods
         </div>
     )
 }
