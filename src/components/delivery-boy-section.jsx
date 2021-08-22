403a3fe5984411bf55d705f2195efb34a56726cf
Neo.Eskini <mreskini30@gmail.com>
2021-08-22 20:07:03 +0430

    fixd the delivery-boy-section title thing

--- src/components/delivery-boy-section.jsx
+++ src/components/delivery-boy-section.jsx
@@ -2,7 +2,7 @@ import ComponentWrapper from "./component-wrapper"
 
 const DeliveryBoy = () => {
     return (
-        <div className="theme-orange-color display-1 font-weight-bold">
+        <div className="theme-orange-color display-2 font-weight-bold">
             Customers to get delivered their ordered foods
         </div>
     )
