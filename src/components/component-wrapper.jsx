cae6c1a538d8162cfe1902c5a1e15f5c4ee860b7
Neo.Eskini <mreskini30@gmail.com>
2021-08-22 20:06:07 +0430

    fixed the wrapper fiel

--- src/components/component-wrapper.jsx
+++ src/components/component-wrapper.jsx
@@ -3,7 +3,7 @@ const ComponentWrapper = (WrappedComponent) => {
         return (
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-12">
+                    <div className="col-10 mx-auto">
                         <WrappedComponent {...props} />
                     </div>
                 </div>
