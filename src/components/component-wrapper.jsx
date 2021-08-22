fcc745fb7c9651cb2c826ea43fc938d38858186f
Neo.Eskini <mreskini30@gmail.com>
2021-08-22 20:10:33 +0430

    updated the component wrapper file thing

--- src/components/component-wrapper.jsx
+++ src/components/component-wrapper.jsx
@@ -1,8 +1,8 @@
-const ComponentWrapper = (WrappedComponent) => {
+const ComponentWrapper = (WrappedComponent, backgroundColorCssClassName) => {
     return (props) => {
         return (
             <div className="container-fluid">
-                <div className="row">
+                <div className={`row ${backgroundColorCssClassName}`}>
                     <div className="col-10 mx-auto">
                         <WrappedComponent {...props} />
                     </div>
--- src/components/delivery-boy-section.jsx
+++ src/components/delivery-boy-section.jsx
@@ -2,7 +2,7 @@ import ComponentWrapper from "./component-wrapper"
 
 const DeliveryBoy = () => {
     return (
-        <div className>
+        <div>
             <div className="theme-orange-color display-2 font-weight-bold">
                 Customers to get delivered their ordered foods
             </div>
@@ -10,5 +10,5 @@ const DeliveryBoy = () => {
     )
 }
 
-const DeliveryBoySection = ComponentWrapper(DeliveryBoy)
+const DeliveryBoySection = ComponentWrapper(DeliveryBoy, "delivery-boy-section-bg")
 export default DeliveryBoySection
