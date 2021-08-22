0871dcb8738b4bd4995ee31c3576ee0c82b91b51
Neo.Eskini <mreskini30@gmail.com>
2021-08-22 20:15:05 +0430

    using the new image thing

--- src/components/delivery-boy-section.jsx
+++ src/components/delivery-boy-section.jsx
@@ -1,10 +1,15 @@
 import ComponentWrapper from "./component-wrapper"
-
+import deliveryBoyImage from "../assets/img/delivery-boy.png"
 const DeliveryBoy = () => {
     return (
         <div className="py-5">
             <div className="theme-orange-color display-2 font-weight-bold">
                 Customers to get delivered their ordered foods
+            </div>
+            <div className="row">
+                <div className="col-lg-6">
+                    <img src={deliveryBoyImage} alt="Delivery Boy Image" />
+                </div>
             </div>
         </div>
     )
