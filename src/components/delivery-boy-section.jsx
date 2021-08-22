f03a2bf7236626d2ccfc4638e16bd09c4eb903f0
Neo.Eskini <mreskini30@gmail.com>
2021-08-22 20:13:42 +0430

    added some padding for the whole delivery-boy-section

--- src/components/delivery-boy-section.jsx
+++ src/components/delivery-boy-section.jsx
@@ -2,7 +2,7 @@ import ComponentWrapper from "./component-wrapper"
 
 const DeliveryBoy = () => {
     return (
-        <div>
+        <div className="py-5">
             <div className="theme-orange-color display-2 font-weight-bold">
                 Customers to get delivered their ordered foods
             </div>
