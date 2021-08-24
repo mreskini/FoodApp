1409f67742a2018dfd882f97d61653d93fa3385d
Neo.Eskini <mreskini30@gmail.com>
2021-08-25 01:55:42 +0430

    change the prettier config

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -1,27 +1,35 @@
-import ComponentWrapper from "./component-wrapper"
-import wavesImage from "../assets/img/waves.png"
-const Hero = () => {
-  return (
-    <div className="py-5 col-12">
-      <div className="row">
-        <div className="col-lg-8">
-          <div className="display-1 font-weight-600 hero-main-title">
-            Always choose happy food
-          </div>
-          <img src={wavesImage} alt="Waves" className="my-5" />
-          <p className="text-white font-karla font-weight-500 fs-20">
-            These tasty certified USDA Organic eggs come from farms that have
-            been pesticide-free for at least three years and from free range
-            flocks that receive no antibiotics
-          </p>
-        </div>
-        <div className="col-lg-4">
+import ComponentWrapper from "./component-wrapper";
+import wavesImage from "../assets/img/waves.png";
+import sandwichImage from "../assets/img/sandwich.png";
 
+const Hero = () => {
+    return (
+        <div className="py-5 col-12">
+            <div className="row">
+                <div className="col-lg-8">
+                    <div className="display-1 font-weight-600 hero-main-title">
+                        Always choose happy food
+                    </div>
+                    <img
+                        src={wavesImage}
+                        alt="Waves"
+                        className="my-5 img-fluid"
+                    />
+                    <p className="text-white font-karla font-weight-500 fs-20">
+                        These tasty certified USDA Organic eggs come from farms
+                        that have been pesticide-free for at least three years
+                        and from free range flocks that receive no antibiotics
+                    </p>
+                </div>
+            </div>
+            <img
+                src={sandwichImage}
+                alt="Sandwich"
+                className="img-fluid float-right"
+            />
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
-const HeroSection = ComponentWrapper(Hero, "hero-section-bg")
-export default HeroSection
+const HeroSection = ComponentWrapper(Hero, "hero-section-bg");
+export default HeroSection;
