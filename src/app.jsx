d056ec57c88b4fc6e5ef6ae31cd818678d683075
Neo.Eskini <mreskini30@gmail.com>
2021-08-25 00:57:48 +0430

    added the prepros.config file thing

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,6 @@ import DeliveryBoySection from "./components/delivery-boy-section"
 const App = () => {
     return (
         <>
-            
             <DeliveryBoySection />
         </>
     )
