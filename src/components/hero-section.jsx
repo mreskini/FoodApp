00597ab0cf2db09391c694bef0c29b6d83451340
Neo.Eskini <mreskini30@gmail.com>
2021-08-25 02:11:38 +0430

    added the requirements for the navigation

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -3,47 +3,52 @@ import wavesImage from "../assets/img/waves.png"
 import sandwichImage from "../assets/img/sandwich.png"
 import googlePlay from "../assets/img/google-play.png"
 import appStore from "../assets/img/app-store.png"
+import Navigation from "./navigation"
 
 const Hero = () => {
     return (
-        <div className="py-5 col-12">
-            <div className="row">
-                <div className="col-lg-7">
-                    <div className="display-1 font-weight-600 hero-main-title">
-                        Always choose happy food
-                    </div>
-                    <img
-                        src={wavesImage}
-                        alt="Waves"
-                        className="my-5 img-fluid"
-                    />
-                    <p className="text-white font-karla font-weight-500 fs-20">
-                        These tasty certified USDA Organic eggs come from farms
-                        that have been pesticide-free for at least three years
-                        and from free range flocks that receive no antibiotics
-                    </p>
-                    <div className="mt-5">
+        <>
+            <Navigation />
+            <div className="py-5 col-12">
+                <div className="row">
+                    <div className="col-lg-7">
+                        <div className="display-1 font-weight-600 hero-main-title">
+                            Always choose happy food
+                        </div>
                         <img
-                            src={appStore}
-                            alt="App Store"
-                            className="img-fluid mr-3"
+                            src={wavesImage}
+                            alt="Waves"
+                            className="my-5 img-fluid"
                         />
+                        <p className="text-white font-karla font-weight-500 fs-20">
+                            These tasty certified USDA Organic eggs come from
+                            farms that have been pesticide-free for at least
+                            three years and from free range flocks that receive
+                            no antibiotics
+                        </p>
+                        <div className="mt-5">
+                            <img
+                                src={appStore}
+                                alt="App Store"
+                                className="img-fluid mr-3"
+                            />
+                            <img
+                                src={googlePlay}
+                                alt="Google Play"
+                                className="img-fluid"
+                            />
+                        </div>
+                    </div>
+                    <div className="col-lg-5">
                         <img
-                            src={googlePlay}
-                            alt="Google Play"
-                            className="img-fluid"
+                            src={sandwichImage}
+                            alt="Sandwich"
+                            className="img-fluid float-right"
                         />
                     </div>
-                </div>
-                <div className="col-lg-5">
-                    <img
-                        src={sandwichImage}
-                        alt="Sandwich"
-                        className="img-fluid float-right"
-                    />
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,3 +1,5 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+
 export default function Navigation() {
     return (
         <Navbar bg="light" expand="lg">
@@ -8,21 +10,6 @@ export default function Navigation() {
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">
-                                Action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">
-                                Something
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
