ad08730cc449948c78848194610c1f672a1d1c4b
Neo.Eskini <mreskini30@gmail.com>
2021-08-25 01:25:12 +0430

    added some new image thing

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -1,12 +1,12 @@
-import ComponentWrapper from "./component-wrapper"
+import ComponentWrapper from "./component-wrapper";
 
 const Hero = () => {
-    return (
-        <div className="py-5 col-12">
-            Some Data thing will go here
-        </div>
-    )
-}
+  return (
+    <div className="py-5 col-12">
+      <div className="display-1 font-weight-bold">Always choose happy food</div>
+    </div>
+  );
+};
 
-const HeroSection = ComponentWrapper(Hero, "hero-section-bg")
-export default HeroSection
+const HeroSection = ComponentWrapper(Hero, "hero-section-bg");
+export default HeroSection;
