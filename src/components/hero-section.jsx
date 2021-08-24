25591a0ce25e85d1cdb975edf17c006e828d9ea8
Neo.Eskini <mreskini30@gmail.com>
2021-08-25 01:41:11 +0430

    added some new font thing for the index.html file

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -1,12 +1,19 @@
 import ComponentWrapper from "./component-wrapper";
-import wavesImage from "../assets/img/waves.png"
+import wavesImage from "../assets/img/waves.png";
 const Hero = () => {
   return (
     <div className="py-5 col-12">
       <div className="row">
-        <div className="col-lg-8 display-1 font-weight-600 hero-main-title">
-          Always choose happy food
-          <img src={wavesImage} alt="Waves" />
+        <div className="col-lg-8">
+          <div className="display-1 font-weight-600 hero-main-title">
+            Always choose happy food
+          </div>
+          <img src={wavesImage} alt="Waves" className="my-5" />
+          <p className="text-white">
+            These tasty certified USDA Organic eggs come from farms that have
+            been pesticide-free for at least three years and from free range
+            flocks that receive no antibiotics
+          </p>
         </div>
       </div>
     </div>
