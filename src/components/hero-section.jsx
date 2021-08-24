61e8301e7db6b00cdb1f3575760c297c33cb9d07
Neo.Eskini <mreskini30@gmail.com>
2021-08-25 01:37:12 +0430

    added the waves image thing for the hero

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -1,11 +1,12 @@
 import ComponentWrapper from "./component-wrapper";
-
+import wavesImage from "../assets/img/waves.png"
 const Hero = () => {
   return (
     <div className="py-5 col-12">
       <div className="row">
         <div className="col-lg-8 display-1 font-weight-600 hero-main-title">
           Always choose happy food
+          <img src={wavesImage} alt="Waves" />
         </div>
       </div>
     </div>
