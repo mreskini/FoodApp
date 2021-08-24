da8d727427fba4ef8988a03a320a605d2fc5382e
Neo.Eskini <mreskini30@gmail.com>
2021-08-25 01:22:11 +0430

    added some styles for the hero background thing

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -2,7 +2,7 @@ import ComponentWrapper from "./component-wrapper"
 
 const Hero = () => {
     return (
-        <div className="py-5">
+        <div className="py-5 col-12">
             Some Data thing will go here
         </div>
     )
