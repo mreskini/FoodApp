d254b9b9b6ac7b4c6ec3f592b78e90c98e7fa119
Neo.Eskini <mreskini30@gmail.com>
2021-08-26 02:58:20 +0430

    added another item thing to change the content later

--- src/components/how-to-section.jsx
+++ src/components/how-to-section.jsx
@@ -7,15 +7,27 @@ const HowTo = () => {
                 <span className="how-to-section-title">How it works</span>
             </div>
             <div className="py-5">
-                <div className="col-lg-3 text-center">
-                    <BsSearch className="display-1 mb-4" />
-                    <h1 className="how-to-section-title h3 font-weight-600 text-center my-4">
-                        Search Resturant
-                    </h1>
-                    <p className="text-white text-center font-karla">
-                        Each time a digital asset is purchased or sold, Sequoir
-                        donates a percentage of the fees back
-                    </p>
+                <div className="row">
+                    <div className="col-lg-3 text-center">
+                        <BsSearch className="display-1 mb-4" />
+                        <h1 className="how-to-section-title h3 font-weight-600 text-center my-4">
+                            Search Resturant
+                        </h1>
+                        <p className="text-white text-center font-karla">
+                            Each time a digital asset is purchased or sold,
+                            Sequoir donates a percentage of the fees back
+                        </p>
+                    </div>
+                    <div className="col-lg-3 text-center">
+                        <BsSearch className="display-1 mb-4" />
+                        <h1 className="how-to-section-title h3 font-weight-600 text-center my-4">
+                            Search Resturant
+                        </h1>
+                        <p className="text-white text-center font-karla">
+                            Each time a digital asset is purchased or sold,
+                            Sequoir donates a percentage of the fees back
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
