5e92449f909d92ee22238f90b3f1046f4409d213
Neo.Eskini <mreskini30@gmail.com>
2021-08-26 02:53:33 +0430

    added the first how-to item

--- src/components/how-to-section.jsx
+++ src/components/how-to-section.jsx
@@ -1,13 +1,25 @@
 import ComponentWrapper from "./component-wrapper"
-import howToBackgroundImage from "../assets/img/how-to-bg.png"
-
+import { BsSearch } from "react-icons/bs"
 const HowTo = () => {
     return (
-            <div className="py-5 my-5">
-                <div className="display-4 font-weight-bold text-center">
-                    <span className="how-to-section-title">How it works</span>
+        <div className="py-5 my-5">
+            <div className="display-4 font-weight-bold text-center">
+                <span className="how-to-section-title">How it works</span>
+            </div>
+
+            <div>
+                <div className="col-lg-3">
+                    <BsSearch className="display-1" />
+                    <h1 className="how-to-section-title h4 text-center my-4">
+                        Search Resturant
+                    </h1>
+                    <p className="text-white text-center font-karla">
+                        Each time a digital asset is purchased or sold, Sequoir
+                        donates a percentage of the fees back
+                    </p>
                 </div>
             </div>
+        </div>
     )
 }
 
