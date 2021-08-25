04517d7b4450bd9db259764b68d5933f9780e2c7
Neo.Eskini <mreskini30@gmail.com>
2021-08-26 02:57:24 +0430

    fixed the style of the how-to section

--- src/components/how-to-section.jsx
+++ src/components/how-to-section.jsx
@@ -3,14 +3,13 @@ import { BsSearch } from "react-icons/bs"
 const HowTo = () => {
     return (
         <div className="py-5 my-5">
-            <div className="display-4 font-weight-bold text-center">
+            <div className="display-4 font-weight-600 text-center">
                 <span className="how-to-section-title">How it works</span>
             </div>
-
-            <div>
-                <div className="col-lg-3">
-                    <BsSearch className="display-1" />
-                    <h1 className="how-to-section-title h4 text-center my-4">
+            <div className="py-5">
+                <div className="col-lg-3 text-center">
+                    <BsSearch className="display-1 mb-4" />
+                    <h1 className="how-to-section-title h3 font-weight-600 text-center my-4">
                         Search Resturant
                     </h1>
                     <p className="text-white text-center font-karla">
