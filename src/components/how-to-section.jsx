d02159751e32542e73bb966ee746bf058e75720f
Neo.Eskini <mreskini30@gmail.com>
2021-08-26 03:07:27 +0430

    added the .margin-top-100 class thing

--- src/components/how-to-section.jsx
+++ src/components/how-to-section.jsx
@@ -2,41 +2,43 @@ import ComponentWrapper from "./component-wrapper"
 import { BsSearch, BsLayoutTextSidebar, BsReplyAll } from "react-icons/bs"
 const HowTo = () => {
     return (
-        <div className="py-5 my-5">
-            <div className="h1 font-weight-600 text-center my-5 py-5">
-                <span className="how-to-section-title">How it works</span>
-            </div>
-            <div className="my-5 py-5">
-                <div className="row pb-5 mb-5">
-                    <div className="col-lg-4 text-center px-5">
-                        <BsSearch className="display-2 mb-4" />
-                        <h1 className="how-to-section-title h3 font-weight-600 text-center my-4">
-                            Search Resturant
-                        </h1>
-                        <p className="text-white text-center font-karla">
-                            Each time a digital asset is purchased or sold,
-                            Sequoir donates a percentage of the fees back
-                        </p>
-                    </div>
-                    <div className="col-lg-4 text-center px-5">
-                        <BsLayoutTextSidebar className="display-2 mb-4" />
-                        <h1 className="how-to-section-title h3 font-weight-600 text-center my-4">
-                            Order Food
-                        </h1>
-                        <p className="text-white text-center font-karla">
-                            Each time a digital asset is purchased or sold,
-                            Sequoir donates a percentage of the fees back
-                        </p>
-                    </div>
-                    <div className="col-lg-4 text-center px-5">
-                        <BsReplyAll className="display-2 mb-4" />
-                        <h1 className="how-to-section-title h3 font-weight-600 text-center my-4">
-                            Deliver to You
-                        </h1>
-                        <p className="text-white text-center font-karla">
-                            Each time a digital asset is purchased or sold,
-                            Sequoir donates a percentage of the fees back
-                        </p>
+        <div className="margin-bottom-100">
+            <div className="py-5 my-5">
+                <div className="h1 font-weight-600 text-center my-5 py-5">
+                    <span className="how-to-section-title">How it works</span>
+                </div>
+                <div className="my-5 py-5">
+                    <div className="row pb-5 mb-5">
+                        <div className="col-lg-4 text-center px-5">
+                            <BsSearch className="display-2 mb-4" />
+                            <h1 className="how-to-section-title h3 font-weight-600 text-center my-4">
+                                Search Resturant
+                            </h1>
+                            <p className="text-white text-center font-karla">
+                                Each time a digital asset is purchased or sold,
+                                Sequoir donates a percentage of the fees back
+                            </p>
+                        </div>
+                        <div className="col-lg-4 text-center px-5">
+                            <BsLayoutTextSidebar className="display-2 mb-4" />
+                            <h1 className="how-to-section-title h3 font-weight-600 text-center my-4">
+                                Order Food
+                            </h1>
+                            <p className="text-white text-center font-karla">
+                                Each time a digital asset is purchased or sold,
+                                Sequoir donates a percentage of the fees back
+                            </p>
+                        </div>
+                        <div className="col-lg-4 text-center px-5">
+                            <BsReplyAll className="display-2 mb-4" />
+                            <h1 className="how-to-section-title h3 font-weight-600 text-center my-4">
+                                Deliver to You
+                            </h1>
+                            <p className="text-white text-center font-karla">
+                                Each time a digital asset is purchased or sold,
+                                Sequoir donates a percentage of the fees back
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
