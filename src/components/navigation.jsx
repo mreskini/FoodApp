bdd48e9761d5bfb813ae6305cb5f2c428e382016
Neo.Eskini <mreskini30@gmail.com>
2021-08-26 02:11:12 +0430

    using the navigagtion-bar-item class thing

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -10,19 +10,19 @@ export default function Navigation() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#" className="bold">
+                        <Nav.Link href="#" className="navigation-bar-item">
                             Home
                         </Nav.Link>
-                        <Nav.Link href="#" className="bold">
+                        <Nav.Link href="#" className="navigation-bar-item">
                             Pages
                         </Nav.Link>
-                        <Nav.Link href="#" className="bold">
+                        <Nav.Link href="#" className="navigation-bar-item">
                             About Us
                         </Nav.Link>
-                        <Nav.Link href="#" className="bold">
+                        <Nav.Link href="#" className="navigation-bar-item">
                             How?
                         </Nav.Link>
-                        <Nav.Link href="#" className="bold">
+                        <Nav.Link href="#" className="navigation-bar-item">
                             Overview
                         </Nav.Link>
                     </Nav>
