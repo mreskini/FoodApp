514212cd3b41567aa2d655e22a9c4f505ba88b2d
Neo.Eskini <mreskini30@gmail.com>
2021-08-26 03:55:04 +0430

    made the move to top button thing working

--- src/components/move-to-top.jsx
+++ src/components/move-to-top.jsx
@@ -2,7 +2,10 @@ import { BsChevronUp } from "react-icons/bs"
 
 const MoveToTop = () => {
     return (
-        <div className="btn btn-lg btn-move-to-top sticky-btn">
+        <div
+            className="btn btn-lg btn-move-to-top sticky-btn"
+            onClick={() => window.scrollTo(0, 0)}
+        >
             <BsChevronUp />
         </div>
     )
