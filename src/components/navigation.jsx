e529d549721dcc9973a71838fb1965cd7fa62127
Neo.Eskini <mreskini30@gmail.com>
2021-08-26 01:33:48 +0430

    added some more content for the navigation.jsx file thing

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -10,8 +10,11 @@ export default function Navigation() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link href="#">Home</Nav.Link>
+                        <Nav.Link href="#">Pages</Nav.Link>
+                        <Nav.Link href="#">About Us</Nav.Link>
+                        <Nav.Link href="#">How?</Nav.Link>
+                        <Nav.Link href="#">Overview</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
