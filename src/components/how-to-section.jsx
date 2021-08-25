726e6ce0d56fa3bdc61b112165b38269f0174fe2
Neo.Eskini <mreskini30@gmail.com>
2021-08-26 02:59:42 +0430

    fixed the new item thing

--- src/components/how-to-section.jsx
+++ src/components/how-to-section.jsx
@@ -1,15 +1,15 @@
 import ComponentWrapper from "./component-wrapper"
-import { BsSearch } from "react-icons/bs"
+import { BsSearch, BsLayoutTextSidebar } from "react-icons/bs"
 const HowTo = () => {
     return (
         <div className="py-5 my-5">
             <div className="display-4 font-weight-600 text-center">
                 <span className="how-to-section-title">How it works</span>
             </div>
-            <div className="py-5">
+            <div className="my-5 py-5">
                 <div className="row">
                     <div className="col-lg-3 text-center">
-                        <BsSearch className="display-1 mb-4" />
+                        <BsSearch className="display-2 mb-4" />
                         <h1 className="how-to-section-title h3 font-weight-600 text-center my-4">
                             Search Resturant
                         </h1>
@@ -19,9 +19,9 @@ const HowTo = () => {
                         </p>
                     </div>
                     <div className="col-lg-3 text-center">
-                        <BsSearch className="display-1 mb-4" />
+                        <BsLayoutTextSidebar className="display-2 mb-4" />
                         <h1 className="how-to-section-title h3 font-weight-600 text-center my-4">
-                            Search Resturant
+                            Order Food
                         </h1>
                         <p className="text-white text-center font-karla">
                             Each time a digital asset is purchased or sold,
