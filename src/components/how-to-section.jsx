13c214de6b35cb6381f3b88e29fd2427cc855ed2
Neo.Eskini <mreskini30@gmail.com>
2021-08-26 02:48:37 +0430

    added the react-icons package thing

--- src/components/how-to-section.jsx
+++ src/components/how-to-section.jsx
@@ -3,13 +3,11 @@ import howToBackgroundImage from "../assets/img/how-to-bg.png"
 
 const HowTo = () => {
     return (
-        <>
             <div className="py-5 my-5">
                 <div className="display-4 font-weight-bold text-center">
                     <span className="how-to-section-title">How it works</span>
                 </div>
             </div>
-        </>
     )
 }
 
