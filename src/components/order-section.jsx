09a34e3d803576a641cc941b513ee62717e4f660
Neo.Eskini <mreskini30@gmail.com>
2021-08-27 14:09:28 +0430

    changed the order section image position

--- src/components/order-section.jsx
+++ src/components/order-section.jsx
@@ -5,32 +5,22 @@ const Order = () => {
         <div className="py-5 my-5">
             <div className="row py-5">
                 <div className="col-lg-6 my-auto">
-                    <h4 className="theme-title-color font-weight-600 my-3">
-                        Menu Management
-                    </h4>
-                    <p className="theme-content-color font-weight-400 line-height-30">
-                        Effort into each unique project knowing that the more we
-                        put into it, the more we get out of it.
+                    <p className="theme-content-color font-weight-500">
+                        What is this?
                     </p>
-                    <h4 className="theme-title-color font-weight-600 my-3">
-                        Food Management
+                    <h4 className="theme-title-color h1 font-weight-600 my-3">
+                        Online Ordering Systems & Mobile Apps for Restaurant
                     </h4>
-                    <p className="theme-content-color font-weight-400 line-height-30">
-                        Effort into each unique project knowing that the more we
-                        put into it, the more we get out of it.
-                    </p>
-                    <h4 className="theme-title-color font-weight-600 my-3">
-                        Delivery Management
-                    </h4>
-                    <p className="theme-content-color font-weight-400 line-height-30">
-                        Effort into each unique project knowing that the more we
-                        put into it, the more we get out of it.
+                    <p className="theme-content-color font-weight-500">
+                        Each time a digital asset is purchased or sold, Sequoir
+                        donates a percentage of the fees back into the
+                        development
                     </p>
                 </div>
-                <div className="col-lg-6">
+                <div className="col-lg-6 text-right">
                     <img
                         src={orderImage}
-                        className="img-fluid pr-3"
+                        className="img-fluid"
                         alt="Delivery Boy"
                     />
                 </div>
