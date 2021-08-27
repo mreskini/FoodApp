8eaee128d0e9df25319f7cd419b44a7992fc01b3
Neo.Eskini <mreskini30@gmail.com>
2021-08-27 14:00:58 +0430

    change the order-component to order-section

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import DeliveryBoySection from "./components/delivery-boy-section";
 import HeroSection from "./components/hero-section";
 import HowToSection from "./components/how-to-section";
 import MoveToTop from "./components/move-to-top";
-import OrderSection from "./components/order-component";
+import OrderSection from "./components/order-section";
 
 const App = () => {
   return (
