bda64636e2babc7fa112da10ed966d87680382ee
Neo.Eskini <mreskini30@gmail.com>
2021-08-27 14:01:10 +0430

    changed the app.jsx indentation thing

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,19 @@
-import DeliveryBoySection from "./components/delivery-boy-section";
-import HeroSection from "./components/hero-section";
-import HowToSection from "./components/how-to-section";
-import MoveToTop from "./components/move-to-top";
-import OrderSection from "./components/order-section";
+import DeliveryBoySection from "./components/delivery-boy-section"
+import HeroSection from "./components/hero-section"
+import HowToSection from "./components/how-to-section"
+import MoveToTop from "./components/move-to-top"
+import OrderSection from "./components/order-section"
 
 const App = () => {
-  return (
-    <>
-      <HeroSection />
-      <DeliveryBoySection />
-      <HowToSection />
-      <OrderSection />
-      <MoveToTop />
-    </>
-  );
-};
+    return (
+        <>
+            <HeroSection />
+            <DeliveryBoySection />
+            <HowToSection />
+            <OrderSection />
+            <MoveToTop />
+        </>
+    )
+}
 
-export default App;
+export default App
